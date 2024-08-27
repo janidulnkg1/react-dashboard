6366911fc94d9ef682f87867ca61e788f3931452
Janidu Lankage <82632348+janidulnkg1@users.noreply.github.com>
2024-08-28 00:36:16 +0530

    add changes

--- cb-data-dashboard/src/app/page.tsx
+++ cb-data-dashboard/src/app/page.tsx
@@ -1,36 +1,40 @@
-import Head from 'next/head'
+"use client"; // Ensure this directive is at the top for a Client Component
+
+import Head from 'next/head';
 import styles from "@/styles/Home.module.css";
 import Dashboard from '@/pages/Dashboard/Dashboard';
 import Login from '@/pages/components/Login';
 import Header from '@/pages/components/Headers/Header';
 import SideMenu from '@/pages/components/SideMenu';
 import Settings from '@/pages/Dashboard/Settings';
-import {useSession} from "next-auth/react";
+import { useSession } from "next-auth/react";
 
-export default function Home() {
+const Home: React.FC = () => {
   const { data: session } = useSession();
 
   return (
     <>
       <Head>
         <title>Data Dashboard</title>
-        <meta name="description" content="Data Dashboard"/>
-        <meta name="viewport" content="width=device-width initial-scale-1"/>
+        <meta name="description" content="Data Dashboard" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-      <Header />
-                {
-                    session && (
-                    <>
-                        <SideMenu />
-                        <Login />
-                    </>
-                    )
-                }
-      <SideMenu />
-        
+        <Header />
+        {
+          session ? (
+            <>
+              <SideMenu />
+              <Dashboard />
+            </>
+          ) : (
+            <Login />
+          )
+        }
       </main>
     </>
   );
 }
+
+export default Home;
